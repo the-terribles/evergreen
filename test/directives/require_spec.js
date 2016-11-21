1a8b3e3e2fa0ec70d04564a34dfe48b74c45f1d0
eduponte <eduponte@users.noreply.github.com>
2016-11-21 11:21:45 +0100

    test(directives): add coverage for error cases

--- test/directives/require_spec.js
+++ test/directives/require_spec.js
@@ -7,7 +7,12 @@ var _ = require('lodash'),
   errors = require('../../lib/errors'),
   expect = chai.expect,
   RequireLoader = require('../../lib/directives/require'),
-  DirectiveContext = require('../../lib/directive-context');
+  DirectiveContext = require('../../lib/directive-context'),
+  errors = require('../../lib/errors'),
+  FileNotFoundError =  errors.FileNotFoundError,
+  NotAFileError =  errors.NotAFileError,
+  JavaScriptFileLoadError =  errors.JavaScriptFileLoadError,
+  NotAJavaScriptFileError = errors.NotAJavaScriptFileError;
 
 describe('Directives', function() {
 
@@ -23,6 +28,58 @@ describe('Directives', function() {
         expect(_context.value).to.deep.eq({ foo: 6 });
         next();
       });
+    });
+
+    it('should throw an error if the file does not exist', function(next){
+
+      var requireLoader = new RequireLoader(),
+          context = new DirectiveContext('file', Path.join(__dirname, './data/unexisting-file-test.js'), []);
+
+      requireLoader.handle(context, {}, {}, function(err, _context){
+        expect(err).not.to.be.null;
+        expect(err).to.be.an.instanceof(FileNotFoundError);
+        next();
+      });
+
+    });
+
+    it('should throw an error if the file is not a javascript file', function(next){
+
+      var requireLoader = new RequireLoader(),
+          context = new DirectiveContext('file', Path.join(__dirname, './data/file-test.txt'), []);
+
+      requireLoader.handle(context, {}, {}, function(err, _context){
+        expect(err).not.to.be.null;
+        expect(err).to.be.an.instanceof(NotAJavaScriptFileError);
+        next();
+      });
+
+    });
+
+    it('should throw an error if the file is not a file', function(next){
+
+      var requireLoader = new RequireLoader(),
+          context = new DirectiveContext('file', Path.join(__dirname, './data'), []);
+
+      requireLoader.handle(context, {}, {}, function(err, _context){
+        expect(err).not.to.be.null;
+        expect(err).to.be.an.instanceof(NotAFileError);
+        next();
+      });
+
+    });
+
+    it('should throw an error if the file is not a file', function(next){
+
+      var requireLoader = new RequireLoader(),
+          context = new DirectiveContext('file', Path.join(__dirname, './data/buggy-file-test.js'), []);
+
+      requireLoader.handle(context, {}, {}, function(err, _context){
+        expect(err).not.to.be.null;
+        expect(err).to.be.an.instanceof(JavaScriptFileLoadError);
+        next();
+      });
+
     });
 
   });
